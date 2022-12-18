413639549cd5fc8e993eab2d2813d310d4d41619
Herlon Aguiar <me@herlon.dev>
2022-12-18 13:24:09 +0100

    plugins: rollback to lsp

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,27 +1,15 @@
--- Load CoC-Nvim custom configs
-require "custom.plugins.coc"
-
 local overrides = require "custom.plugins.overrides"
 
 return {
   -- Enable dashboard
   ["goolord/alpha-nvim"] = { disable = false },
 
-  -- CoC NVIM
-  ["neoclide/coc.nvim"] = {
-    branch = "release",
+  ["neovim/nvim-lspconfig"] = {
+      config = function()
+        require "plugins.configs.lspconfig"
+        require "custom.plugins.lspconfig"
+      end,
   },
-
-  -- Disable lsp and cmp
-  ["neovim/nvim-lspconfig"] = false,
-  ["hrsh7th/nvim-cmp"] = false,
-  ["hrsh7th/cmp-nvim-lua"] = false,
-  ["hrsh7th/cmp-nvim-lsp"] = false,
-  ["hrsh7th/cmp-buffer"] = false,
-  ["hrsh7th/cmp-path"] = false,
-  ["saadparwaiz1/cmp_luasnip"] = false,
-  ["L3MON4D3/LuaSnip"] = false,
-  ["windwp/nvim-autopairs"] = false,
 
   -- overrde plugin configs
   ["nvim-treesitter/nvim-treesitter"] = {
